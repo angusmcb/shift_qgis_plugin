91b0721df5d41587e1b6a270c5ec68d4739375e7
Angus <gitcommit@zog.mozmail.com>
2026-04-16 19:57:17 +0200

    Create first test

---
+++ test/test_plugin.py
@@ -0,0 +1,33 @@
+
+
+from qgis import processing
+
+def test_plugin(qgis_processing,qgis_iface):
+    import shift
+
+    x_min = 776302
+    y_min=214895
+    x_max=831861
+    y_max= 254345
+
+    x_shift=50000
+    y_shift=-10000
+
+    plugin = shift.classFactory(qgis_iface)
+
+    plugin.initGui()
+
+    create_outputs = processing.run("native:createrandomnormalrasterlayer", {'EXTENT':f'{x_min},{x_max},{y_min},{y_max} [EPSG:27562]','TARGET_CRS':QgsCoordinateReferenceSystem('EPSG:27562'),'PIXEL_SIZE':1000,'OUTPUT_TYPE':0,'MEAN':0,'STDDEV':1,'CREATION_OPTIONS':None,'OUTPUT':'TEMPORARY_OUTPUT'})
+
+    initial_raster=create_outputs['OUTPUT']
+
+    shift_outputs = processing.run("shift:shift", {'INPUT':initial_raster,'X_SHIFT':x_shift,'Y_SHIFT':y_shift,'OUTPUT':'TEMPORARY_OUTPUT'})
+
+    out_layer = QgsRasterLayer(shift_outputs['OUTPUT'])
+
+    assert out_layer.extent().xMinimum() == x_min + x_shift
+    assert out_layer.extent().xMaximum() == x_max + x_shift
+    assert out_layer.extent().yMinimum() == y_min + y_shift
+    assert out_layer.extent().yMaximum() == y_max +y_shift
+
+    plugin.unload()
